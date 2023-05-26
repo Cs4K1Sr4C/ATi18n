437ce21391efe6bd7732fc0baa2da79bce119f41
Cs4K1Sr4C <cs4k1sr4c@tutanota.com>
2023-05-26 06:22:45 +0200

    :recycle: Settings menu separation

---
+++ src/agents/TerminalAssistant/settings.ts
@@ -0,0 +1,16 @@
+import * as p from "@clack/prompts";
+import { settingsMenuOptions } from "../../utils/constants";
+
+export const selectSettingsMenu = async () => {
+    console.clear();
+    const settingsMenu = await p.select({
+        message: "[🤖]::> What would you like to do?",
+        options: settingsMenuOptions,
+    });
+    if (settingsMenu === "1") { }
+    else if (settingsMenu === "2") { }
+    else if (settingsMenu === "3") { }
+    else if (settingsMenu === "4") { }
+    else if (settingsMenu === "5") { }
+    else if (settingsMenu === "X") { }
+};
